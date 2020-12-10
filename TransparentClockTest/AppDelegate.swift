a76d9f59e2caff32945374d8f6c4fd6c5b36bf7e
Aye Aye Mon <noblesilence@gmail.com>
2020-12-10 15:26:39 +0700

    Initial Commit

---
+++ TransparentClockTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TransparentClockTest
+//
+//  Created by Aye Aye Mon on 12/10/20.
+//  Copyright © 2020 LTC Clock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
